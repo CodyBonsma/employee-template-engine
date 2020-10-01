dd1c7d7d7522c6f83c1921fe30f7536f83d9ba1c
Cody Bonsma <codybonsma@gmail.com>
2020-09-30 22:00:37 -0400

    took a screenshot of an example. created a readme

--- Develop/app.js
+++ Develop/app.js
@@ -11,6 +11,17 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 const employeeArray = [];
+
+
+
+
+
+
+
+
+
+
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
